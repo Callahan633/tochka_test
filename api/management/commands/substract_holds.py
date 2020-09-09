d7f1e49fb1b7463569346ba617137f43ea9fd212
Ivan <moritcsh@gmail.com>
2020-09-09 16:10:51 +0500

    fix management command for substracting holds

--- api/management/commands/substract_holds.py
+++ api/management/commands/substract_holds.py
@@ -6,6 +6,8 @@ from api.models import Account
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for item in Account.objects.all():
-            if item.is_opened is True:
-                item.current_balance -= item.reserved_operations
-                item.reserved_operations = 0
+            if item.is_opened:
+                if item.current_balance - item.reserved_operations >= 0:
+                    item.current_balance -= item.reserved_operations
+                    item.reserved_operations = 0
+                    item.save()
